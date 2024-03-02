13c6c1936e08913dcf37668b014db1071db96fbe
mamunur-roshid <nijummamun7@gmail.com>
2024-03-03 01:43:27 +0600

    first-commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to toggle dropdown visibility
+function toggleDropdown(dropdownId) {
+  var dropdown = document.getElementById(dropdownId);
+  if (dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+  } else {
+    dropdown.style.display = "block";
+  }
+}
